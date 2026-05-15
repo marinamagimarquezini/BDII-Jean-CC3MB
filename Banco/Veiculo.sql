4718f79fbdc34877d7dc2d76436c3a1adc82d1d3
Leonardo Goronci Zanotti <leonardouvv00@gmail.com>
2026-05-15 17:33:17 -0300

    Insert vehicle records with client relationships
    Added vehicle entries with associated client CPF.

--- Banco/Veiculo.sql
+++ Banco/Veiculo.sql
@@ -1,10 +1,11 @@
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('ABC-1234', null, 'Electra', 'Crimson');
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('DEF-4567', null, 'Ram Wagon B150', 'Turquoise');
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('MNO-7890', null, 'Element', 'Khaki');
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('MNO-7890', null, 'Murano', 'Orange');
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('PQR-0123', null, 'Probe', 'Maroon');
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('QWE-9012', null, 'Golf III', 'Khaki');
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('QWE-9012', null, 'SLK-Class', 'Crimson');
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('RST-2345', null, 'Land Cruiser', 'Goldenrod');
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('MNO-7890', null, 'S-Class', 'Orange');
-insert into Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) values ('XYZ-5678', null, 'Mustang', 'Red');
+-- Relacionando os veículos aos clientes através do cpf_Cliente
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('ABC-1234', '848-76-7247', 'Electra', 'Crimson');
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('DEF-4567', '634-02-5146', 'Ram Wagon B150', 'Turquoise');
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('MNO-7890', '808-59-1906', 'Element', 'Khaki');
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('GHI-3456', '207-83-8823', 'Murano', 'Orange');
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('PQR-0123', '120-08-9255', 'Probe', 'Maroon');
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('QWE-9012', '800-74-6056', 'Golf III', 'Khaki');
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('STU-0123', '524-45-3750', 'SLK-Class', 'Crimson');
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('RST-2345', '662-41-6672', 'Land Cruiser', 'Goldenrod');
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('VWX-4567', '491-10-4131', 'S-Class', 'Orange');
+INSERT INTO Veiculo (placa_veiculo, Cliente_cpf_Cliente, modelo, cor) VALUES ('XYZ-5678', '139-17-5596', 'Mustang', 'Red');
